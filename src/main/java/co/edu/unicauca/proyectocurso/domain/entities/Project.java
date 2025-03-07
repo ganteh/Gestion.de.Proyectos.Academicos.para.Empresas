f2f5aa6921a0f9c46713526a332ed6795efb7652
ganteh <cristobalvillaquiran@gmail.com>
2025-03-06 23:37:28 -0500

    Cambios en atributos de proyecto e implementación de clase Task(tarea) y TaskState.

--- src/main/java/co/edu/unicauca/proyectocurso/domain/entities/Project.java
+++ src/main/java/co/edu/unicauca/proyectocurso/domain/entities/Project.java
@@ -4,33 +4,29 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.Date;
 
 public class Project {
     private UUID id;
     private String name;
-    private String summary;
-    private String objectives;
     private String description;
-    private int maxMonths;
-    private Double budget;
+    private Date FinalizationDate;
     private LocalDate date;
     private ProjectState state;
     private Company company;
     private List<String> comments;
+    private List<Student> students;
+    private List<Task> tasks;
 
-    public Project(String name, String summary, String objectives, String description, 
-                   int maxMonths, Double budget, Company company) {
+    public Project(String name, String summary, String objectives, String description, int maxMonths, Double budget, Company company) {
         this.id = UUID.randomUUID();
         this.name = name;
-        this.summary = summary;
-        this.objectives = objectives;
         this.description = description;
-        this.maxMonths = maxMonths;
-        this.budget = budget;
         this.date = LocalDate.now();
         this.state = ProjectState.RECEIVED;
         this.company = company;
         this.comments = new ArrayList<>();
+        this.students = new ArrayList<>();
     }
 
     public void addComment(String comment) {
@@ -40,6 +36,10 @@ public class Project {
     public void changeState(ProjectState newState) {
         this.state = newState;
     }
+    
+    public void addStudent(Student student) {
+        students.add(student);
+    }
 
     // Getters y Setters
     public UUID getId() { return id; }
@@ -48,20 +48,8 @@ public class Project {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public String getSummary() { return summary; }
-    public void setSummary(String summary) { this.summary = summary; }
-
-    public String getObjectives() { return objectives; }
-    public void setObjectives(String objectives) { this.objectives = objectives; }
-
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
-
-    public int getMaxMonths() { return maxMonths; }
-    public void setMaxMonths(int maxMonths) { this.maxMonths = maxMonths; }
-
-    public Double getBudget() { return budget; }
-    public void setBudget(Double budget) { this.budget = budget; }
 
     public LocalDate getDate() { return date; }
     public void setDate(LocalDate date) { this.date = date; }
@@ -74,4 +62,13 @@ public class Project {
 
     public List<String> getComments() { return comments; }
     public void setComments(List<String> comments) { this.comments = comments; }
+    
+    public List<Student> getStudents() {return students;}
+    public void setStudents(List<Student> students) {this.students = students;}
+    
+    public List<Task> getTasks() {return tasks;}
+    public void setTasks(List<Task> tasks) {this.tasks = tasks;}
+    
+    public Date getFinalizationDate() {return FinalizationDate;}
+    public void setFinalizationDate(Date FinalizationDate) {this.FinalizationDate = FinalizationDate;}
 }
